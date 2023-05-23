7cf94ba78e94c9ad486244db854a83c3781ab026
Jhongom <jhon.gomez-t@mail.escuelaing.edu.co>
2023-05-23 16:00:44 -0500

    Portfolio

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello 
+
+
+![](./imagenes/port.PNG)
+
---
+++ cod.js
@@ -0,0 +1,16 @@
+let botonConoce = document.getElementById("boton1");
+let botonPort = document.getElementById("boton2");
+let botonMensaje = document.getElementById("boton4");
+
+
+botonConoce.addEventListener("click", function(){
+    window.location.href = 'https://www.linkedin.com/in/jhongomc25/'
+});
+
+botonPort.addEventListener("click", function(){
+    window.location.href = "https://github.com/Jhongom"
+})
+
+botonMensaje.addEventListener("click", function(){
+    window.location.href = "mailto:jhon.gomez-t@mail.escuelaing.edu.co"
+})
